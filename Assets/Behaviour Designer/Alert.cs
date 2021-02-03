6e0eac1e6073f65c78baacc8bc4ec26c48144c0f
stevenwhatever123 <60275831+stevenwhatever123@users.noreply.github.com>
2021-02-03 17:14:55 +0000

    Updated enemy behaviour tree for wilder vision cone
    The A.I will now seek the player and play a small alert animation when the player enters the wilder vision cone

---
+++ Assets/Behaviour Designer/Alert.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using BehaviorDesigner.Runtime;
+using BehaviorDesigner.Runtime.Tasks;
+using UnityEngine;
+
+public class Alert : Action
+{
+    const string k_AnimAlertedParameter = "Alerted";
+
+    public ParticleSystem[] onDetectVFX;
+
+    public AudioClip onDetectSFX;
+
+    public Animator animator;
+
+    public override TaskStatus OnUpdate()
+    {
+        OnDetectedTarget();
+        return TaskStatus.Success;
+    }
+
+    void OnDetectedTarget()
+    {
+
+        for (int i = 0; i < onDetectVFX.Length; i++)
+        {
+            onDetectVFX[i].Play();
+        }
+
+        if (onDetectSFX)
+        {
+            AudioUtility.CreateSFX(onDetectSFX, transform.position, AudioUtility.AudioGroups.EnemyDetection, 1f);
+        }
+
+        animator.SetBool(k_AnimAlertedParameter, true);
+    }
+}
