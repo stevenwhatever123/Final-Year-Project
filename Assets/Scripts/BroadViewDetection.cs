72972568a0147c704a3dfee052f1441e827e929f
stevenwhatever123 <60275831+stevenwhatever123@users.noreply.github.com>
2021-02-03 22:03:23 +0000

    Added collider and scripts and updated Behaviour Tree
    Added capsule collider as board view vision cone. Added scripts to handle trigger enter and exits. Added rotate to player in behaviour tree when player detected is true.

---
+++ Assets/Scripts/BroadViewDetection.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using BehaviorDesigner.Runtime;
+using UnityEngine;
+
+public class BroadViewDetection : MonoBehaviour
+{
+    private BehaviorTree behaviorTree;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        behaviorTree = gameObject.GetComponent<BehaviorTree>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            behaviorTree.SetVariableValue("PlayerDetected", true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            behaviorTree.SetVariableValue("PlayerDetected", false);
+        }
+    }
+}
