f52237ffe2197b352dd33a30006e41ae2a3665cc
stevenwhatever123 <60275831+stevenwhatever123@users.noreply.github.com>
2021-02-10 21:08:51 +0000

    Added Monte carlo node and search
    It does not fully worked yet

---
+++ Assets/FindArea.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class FindArea : MonoBehaviour
+{
+    public GameObject NavMeshSurfaceGameObject;
+
+    public NavMeshSurface surface;
+
+    public GameObject Player;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        NavMeshSurfaceGameObject = GameObject.FindWithTag("NavMesh");
+        surface = NavMeshSurfaceGameObject.GetComponent<NavMeshSurface>();
+
+        Player = GameObject.FindWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log(surface.navMeshData);
+        //Debug.Log(Player.transform.position);
+    }
+}
---
+++ Assets/Scripts/MonteCarloTreeSearch.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MonteCarloTreeSearch : MonoBehaviour
+{
+
+    public int budget = 10;
+
+    private MonteCarloNode leaf;
+    private MonteCarloNode expandedLeaf;
+    private MonteCarloNode simulationResult;
+
+    public Vector3 targetDestination;
+
+    void Update()
+    {
+        MCTS();
+    }
+
+    void MCTS()
+    {
+        int counter = 0;
+        
+        MonteCarloNode rootNode = new MonteCarloNode(transform.position, targetDestination);
+        do
+        {
+            leaf = rootNode.Selection();
+            expandedLeaf = leaf.Expand();
+            simulationResult = expandedLeaf.rollout();
+            simulationResult.backPropogation(expandedLeaf);
+            counter++;
+        } while (counter < budget);
+
+        rootNode.BestLeaf();
+        
+    }
+
+    void draw(MonteCarloNode node)
+    {
+        List<Vector3> path;
+        while (node.hasLeaf())
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawLine(node.currentPosition, node.getLeaf().currentPosition);
+        }
+    }
+}
