224029447b27509c66975c3b7107207400c77f29
stevenwhatever123 <60275831+stevenwhatever123@users.noreply.github.com>
2021-02-26 21:17:27 +0000

    Implemented A-star Pathfinding
    Rearranged file location and added walkable and unwalkable mask layer to walls and floors

---
+++ Assets/Scripts/A-Star/AStarNode.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AStarNode
+{
+    public bool walkable;
+    public Vector3 worldPosition;
+    public int gridX;
+    public int gridY;
+
+    public int gCost;
+    public int hCost;
+
+    public AStarNode parent;
+
+    public AStarNode(bool walkable, Vector3 worldPosition, int gridX, int gridY)
+    {
+        this.walkable = walkable;
+        this.worldPosition = worldPosition;
+        this.gridX = gridX;
+        this.gridY = gridY;
+    }
+
+    public int fCost()
+    {
+        return gCost + hCost;
+    }
+}
---
+++ Assets/Scripts/A-Star/AStarPathfinding.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AStarPathfinding : MonoBehaviour
+{
+    public Transform seeker, target;
+    
+    
+    private AStarGrid grid;
+
+    void Awake()
+    {
+        grid = GetComponent<AStarGrid>();
+    }
+
+    void Update()
+    {
+        FindPath(seeker.position, target.position);
+    }
+
+    void FindPath(Vector3 startPos, Vector3 targetPos)
+    {
+        AStarNode startNode = grid.NodeFromWorldPoint(startPos);
+        AStarNode targetNode = grid.NodeFromWorldPoint(targetPos);
+
+        List<AStarNode> openSet = new List<AStarNode>();
+        HashSet<AStarNode> closedSet = new HashSet<AStarNode>();
+        openSet.Add(startNode);
+
+        while (openSet.Count > 0)
+        {
+            AStarNode currentNode = openSet[0];
+            for (int i = 1; i < openSet.Count; i++)
+            {
+                if (openSet[i].fCost() < currentNode.fCost() || openSet[i].fCost() == currentNode.fCost()
+                        && openSet[i].hCost < currentNode.hCost)
+                {
+                    currentNode = openSet[i];
+                }
+            }
+
+            openSet.Remove(currentNode);
+            closedSet.Add(currentNode);
+
+            if (currentNode == targetNode)
+            {
+                RetracePath(startNode, targetNode);
+                return;
+            }
+
+            foreach (AStarNode neighbour in grid.GetNeighbours(currentNode))
+            {
+                if (!neighbour.walkable || closedSet.Contains(neighbour))
+                {
+                    continue;
+                }
+
+                int newMovementCostToNeighbour = currentNode.gCost + GetDistance(currentNode, neighbour);
+                if (newMovementCostToNeighbour < neighbour.gCost || !openSet.Contains(neighbour))
+                {
+                    neighbour.gCost = newMovementCostToNeighbour;
+                    neighbour.hCost = GetDistance(neighbour, targetNode);
+                    neighbour.parent = currentNode;
+
+                    if (!openSet.Contains(neighbour))
+                    {
+                        openSet.Add(neighbour);
+                    }
+                }
+
+            }
+        }
+    }
+
+    void RetracePath(AStarNode starNode, AStarNode targetNode)
+    {
+        List<AStarNode> path = new List<AStarNode>();
+        AStarNode currentNode = targetNode;
+
+        while (currentNode != starNode)
+        {
+            path.Add(currentNode);
+            currentNode = currentNode.parent;
+        }
+        
+        path.Reverse();
+
+        grid.path = path;
+    }
+
+    int GetDistance(AStarNode nodeA, AStarNode nodeB)
+    {
+        int distanceX = Mathf.Abs(nodeA.gridX - nodeB.gridX);
+        int distanceY = Mathf.Abs(nodeA.gridY - nodeB.gridY);
+
+        if (distanceX > distanceY)
+        {
+            return 14 * distanceY + 10 * (distanceX - distanceY);
+        }
+        else
+        {
+            return 14 * distanceX + 10 * (distanceY - distanceX);
+        }
+    }
+}
